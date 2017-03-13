35ab3ce767d93c05e07da64fe49ffdef57ae7ddd
marcusschweizer <marcus.schweizer@gmail.com>
2017-03-13 20:05:23 +1100

    testing framework
    added testing framework to start running proper unit tests on existing
    and future code in the tests folder.
    Can be rune from main SudokuTrainer.py

--- SudokuTrainer.py
+++ SudokuTrainer.py
@@ -4,12 +4,13 @@ from modules.SudokuSolver import *
 from modules.SudokuGenerator import *
 from modules.fileio import *
 from modules.helper import *
+from tests.SudokuTesting import *
 
-
+import sys, re, optparse
 import time
 
 
-def main():
+def main(options):
 
     clear_terminal()
 
@@ -19,7 +20,7 @@ def main():
     suds = fileio.from_file(data_file)
     suds = [sud for sud in suds if not sud.is_empty()]
 
-    sudoku = suds[4]
+    #sudoku = suds[4]
 
     # print(sudoku.print_board())
 
@@ -29,22 +30,58 @@ def main():
     for sud in suds:
         clear_terminal()
         print(sud.print_board())
-        SudokuSolver.solve(sud, True, 0.05)
-        time.sleep(1)
-    #"""
-    """
-	for sud in suds:
-		print(sud.print_board())
-	#"""
-    """
-	sud3 = Sudoku()
+        if options.debug:
+            SudokuSolver.solve(sud, True, 0.05)
+            time.sleep(1)
+        else:
+            SudokuSolver.solve(sud, True, 0)
+            time.sleep(0.5)
+        
 
-	SudokuGenerator.generate_random(sud3)
-	
-	print(sud3.print_board())
+def process_args():
+    first_re = re.compile(r'^\d{3}$')
 
-	SudokuSolver.solve(sud3, True, 0.2)
-	#"""
+    parser = optparse.OptionParser()
+    parser.set_defaults(test=False, debug=False)
+    parser.add_option('--test', action='store_true', dest='test')
+    parser.add_option('--debug', action='store_true', dest='debug')
+    (options, args) = parser.parse_args()
+
+    if len(args) == 1:
+        if first_re.match(args[0]):
+            print("Primary argument is : ", args[0])
+        else:
+            raise ValueError("First argument should be ...")
+    elif len(args) > 1:
+        raise ValueError("Too many command line arguments")
+
+    if options.test:
+        print('test flag set')
+    
+    if options.debug:
+        print('debug flag set')
+    
+    del sys.argv[1:]
+
+    return options
+
+
+def run_tests():
+    print("Running Test Suite")
+
+    #st = SudokuTesting()
+    unittest.main()
+
+
 
 if __name__ == '__main__':
-    main()
+
+    options = process_args()
+
+    if options.test:
+        run_tests()
+    else:
+        main(options)
+
+
+   
---
+++ tests/SudokuTest.py
@@ -0,0 +1,37 @@
+
+import unittest
+
+"""
+Testing suite for SudokuTraining
+Can be run with either:
+bash: python3 SudokuTrainer.py --test
+bash: python3 -m unittest tests/SudokuTesting.py
+"""
+
+
+
+
+class SudokuTest(unittest.TestCase):
+    """Testing for Sudokus
+    """
+
+    def test_test(self):
+        self.assertTrue(True)
+
+
+class SudokuTest_SudokuSolver(unittest.TestCase):
+
+    def test_test(self):
+        self.assertTrue(True)
+    
+
+class SudokuTest_SudokuGenerator(unittest.TestCase):
+
+    def test_test(self):
+        self.assertTrue(True)
+
+
+class SudokuTest_helper(unittest.TestCase):
+
+    def test_test(self):
+        self.assertTrue(True)
